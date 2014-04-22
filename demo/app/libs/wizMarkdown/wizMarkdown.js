455f68d0ff2882fddb37cc536cf2a20bb1231632
Greg <gregory.pratt@me.com>
2014-04-22 12:58:49 +0100

    Pull request #1, Grunt tasks and re-organisation

--- demo/app/libs/wizMarkdown/wizMarkdown.js
+++ demo/app/libs/wizMarkdown/wizMarkdown.js
@@ -1,4 +1,4 @@
-﻿// 
+// 
 //! MarkdownDeep - http://www.toptensoftware.com/markdowndeep
 //! Copyright (C) 2010-2011 Topten Software
 // 
@@ -5822,23 +5822,24 @@ angular.module('wiz.markdown')
 angular.module('wiz.markdown')
 
 .directive('wizMarkdown', ['$filter', 'wizMarkdownSvc', function ($filter, wizMarkdownSvc) {
-    return {
-        restrict: 'E',
-        scope: {
-            'content': '='
-        },
-        replace: true,
-        template: '<div class="markdown-output"></div>',
-        link: function (scope, elem, attrs) {
-            scope.$watch('content', function () {
-                elem.html(wizMarkdownSvc.Transform(scope.content));
-                // Apply highlighting when required
-                angular.forEach(elem.find('pre'), function(value){
-                    hljs.highlightBlock(value);
-                });
-            });
-        }
-    };
+	return {
+		restrict: 'E',
+		scope: {
+			'content': '='
+		},
+		replace: true,
+		template: '<div class="markdown-output"></div>',
+		link: function (scope, elem, attrs) {
+			scope.$watch('content', function () {
+				if (!scope.content) return null;
+				elem.html(wizMarkdownSvc.Transform(scope.content));
+				// Apply highlighting when required
+				angular.forEach(elem.find('pre'), function (value) {
+					hljs.highlightBlock(value);
+				});
+			});
+		}
+	};
 }]);
 angular.module('wiz.markdown')
 
